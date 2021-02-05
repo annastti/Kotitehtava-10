ef99158633c81e323fcc345994b5ed92e625133c
annastti <78202582+annastti@users.noreply.github.com>
2021-02-05 10:59:02 +0200

    Update index.php
    lisätty br:iä

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
 <body>
 <?php include 'header.php';?>
 <p>Tässä tehtävässä testataan php:n include-komentoa.</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis itaque nam provident consectetur, vero vel nulla neque, quaerat quisquam labore quia quam perferendis sit harum laudantium ad explicabo, reiciendis consequatur..</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
+Corporis itaque nam provident consectetur, vero vel nulla neque, <br>
+quaerat quisquam labore quia quam perferendis sit harum laudantium <br>
+ad explicabo, reiciendis consequatur.</p>
 <?php include 'footer.php';?>
 </body>
 </html>
